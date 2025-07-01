9a6b30ede1b32785f8cd253c7d122625da6f8749
Alejandro-cj <aloflow_345@hotmail.com>
2025-07-01 08:46:56 -0500

    Agregar tarjetas dinámicas a la lista 'Por hacer'

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("TaskFlow cargado correctamente");
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("todo-form");
+  const input = document.getElementById("todo-input");
+  const cardsContainer = document.getElementById("todo-cards");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const taskText = input.value.trim();
+
+    if (taskText !== "") {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.textContent = taskText;
+
+      cardsContainer.appendChild(card);
+      input.value = "";
+    }
+  });
+});
